da800aa35293982f9a00f0b211fd5ab32b206b58
elias-a <elias.alstead@gmail.com>
2024-12-07 01:09:46 -0500

    day7 part1

---
+++ day7/part1.py
@@ -0,0 +1,25 @@
+import os
+from operator import mul, add
+from itertools import product
+
+
+def process(row):
+    test, equation = row.split(":")
+    test = int(test)
+    numbers = [int(n) for n in equation.strip().split(" ")]
+    operators = [mul, add]
+    equations = product(operators, repeat=len(numbers)-1)
+    for eq in equations:
+        acc = numbers[0]
+        for op, num in zip(eq, numbers[1:]):
+            acc = op(acc, num)
+            if acc > test:
+                break
+        if acc == test:
+            return test
+    return 0
+
+
+with open(os.path.join(os.path.dirname(__file__), "input.txt"), "rt") as f:
+    total = sum(process(r.strip()) for r in f)
+    print(total)
